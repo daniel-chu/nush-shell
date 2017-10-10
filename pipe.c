77e71899e76a9094aba304a6576e8c7e158ba00c
daniel-chu <chu.da@husky.neu.edu>
2017-10-09 20:58:39 -0400

    Improved pipe.

--- pipe.c
+++ pipe.c
@@ -19,42 +19,6 @@ get_pipe_index(svec* tokens)
 }
 
 int
-exec_left_side_pipe(int pipes[], svec* tokens) {
-    int cpid;
-    if((cpid = fork())) {
-    } else {
-        close(pipes[0]);
-        // close stdout
-        close(1);
-        // sets pipes[1] to be in file descriptor table as 1
-        dup(pipes[1]);
-
-        execute(tokens);
-        exit(0);
-        // execvp(tokens->data[0], tokens->data);
-    }
-    return 0;
-}
-
-int
-exec_right_side_pipe(int pipes[], svec* tokens) {
-    int cpid;
-    if((cpid = fork())) {
-    } else {
-        close(pipes[1]);
-        // close stdin
-        close(0);
-        // sets pipes[0] to be in file descriptor table as 0
-        dup(pipes[0]);
-
-        execute(tokens);
-        exit(0);
-        // execvp(tokens->data[0], tokens->data);
-    }
-    return 0;
-}
-
-int
 handle_and_exec_pipe(int index, svec* tokens)
 {
     svec* left = svec_subvec(tokens, 0, index);
@@ -67,13 +31,33 @@ handle_and_exec_pipe(int index, svec* tokens)
         exit(1);
     }
 
-    exec_left_side_pipe(pipes, left);
-    exec_right_side_pipe(pipes, right);
+    int cpid1;
+    int cpid2;
+    int status;
 
-    // TODO how to get it to delay printing nush$ immediately
+    if(!(cpid1 = fork())) {
+        close(pipes[0]);
+        // sets pipes[1] to be in file descriptor table as 1
+        dup2(pipes[1], 1);
+
+        execute(left);
+        exit(0);
+    }
+
+    if(!(cpid2 = fork())) {
+        close(pipes[1]);
+        // sets pipes[0] to be in file descriptor table as 0
+        dup2(pipes[0], 0);
+
+        execute(right);
+        exit(0);
+    }
 
     close(pipes[0]);
     close(pipes[1]);
+
+    waitpid(cpid1, &status, 0);
+    waitpid(cpid2, &status, 0);
 
     return 0;
 }
